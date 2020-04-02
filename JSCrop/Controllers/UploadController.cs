fd007948d96ce8cec452d8d6e7426ced58ff4dce
Raphaël Désalbres <raphael.desalbres@outlook.com>
2020-04-02 14:48:18 +0200

    Initial commit

---
+++ JSCrop/Controllers/UploadController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSCrop.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost("Save")]
+        public async Task<IActionResult> Save()
+        {
+            string base64 = Request.Form["imgCropped"];
+            string imageName = Request.Form["fileName"];
+            string imageWidth = Request.Form["imgWidth"];
+            string imageHeight = Request.Form["imgHeight"];
+            var fileName = Path.GetFileNameWithoutExtension(imageName);
+            var fileExt = Path.GetExtension(imageName);
+            var finalFileName = $"{fileName}-{imageWidth}x{imageHeight}{fileExt}";
+            var bytes = Convert.FromBase64String(base64.Split(',')[1]);            
+            var path=Path.Combine(Directory.GetCurrentDirectory(),"Upload", finalFileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+               await stream.WriteAsync(bytes, 0, bytes.Length);
+               stream.Flush();
+            }
+            return Redirect("/Success");
+        }
+        
+    }
+}
---
+++ JSCrop/wwwroot/js/main.js
@@ -0,0 +1,44 @@
+﻿function SetCoordinates(c) {
+    $('#imgX1').val(c.x);
+    $('#imgY1').val(c.y);
+    $('#imgWidth').val(c.w);
+    $('#imgHeight').val(c.h);
+    $('#btnCrop').show();
+};
+
+window.JSCrop = {
+    Init: function () {        
+        $('#FileUpload1').change(function (e) {
+            var fileName = e.target.files[0].name;
+            $('#fileName').val(fileName);
+            $('#Image1').hide();
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#Image1').show();
+                $('#Image1').attr("src", e.target.result);
+                $('#Image1').Jcrop({
+                    onChange: SetCoordinates,
+                    onSelect: SetCoordinates
+                });
+            }
+            reader.readAsDataURL($(this)[0].files[0]);
+        });
+    },
+    Crop: function () {
+        var x1 = $('#imgX1').val();
+        var y1 = $('#imgY1').val();
+        var width = $('#imgWidth').val();
+        var height = $('#imgHeight').val();
+        var canvas = $("#canvas")[0];
+        var context = canvas.getContext('2d');
+        var img = new Image();
+        img.onload = function () {
+            canvas.height = height;
+            canvas.width = width;
+            context.drawImage(img, x1, y1, width, height, 0, 0, width, height);
+            $('#imgCropped').val(canvas.toDataURL());
+            //$('#btnUpload').show();
+        };
+        img.src = $('#Image1').attr("src");
+    }    
+}
